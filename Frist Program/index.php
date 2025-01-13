3be384814d7ef009041d78a233ab03d4de7490a1
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-01-13 15:53:57 +0530

    tryning to push php code in github

---
+++ Frist Program/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Frist Program</title>
+</head>
+<body>
+  <h1>Basic Syntax and Structure in PHP</h1>
+  <?php  
+  
+ //echo "<h2>Hello World</h2>"
+ Echo "<h2>Hello World!!!</h2>";
+ $name = "Sahil";
+ echo "$name <br/>";
+
+//  $person = {
+//   name = 'Mozahid',
+//   age = '21',
+//   course = 'Bsc'
+//  };
+
+$colors = array("Red", "Green", "Blue");
+$animals = ['Cat','Dog','Lion'];
+foreach($animals as $animal){
+  echo "$animal<br/>";
+};
+  
+  ?>
+</body>
+</html>
