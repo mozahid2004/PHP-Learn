f9ad0febd2c10a13cbf645f3eaf668e50d7ad220
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-01-13 23:36:52 +0530

    In this folder I Learn about the types of loopss and its work

---
+++ Loops/loop.php
@@ -0,0 +1,29 @@
+<?php
+
+
+// 1. While Loop
+
+    $num = 1;
+    $tnum = 1.9;
+  
+
+    // while($num<=12){
+      
+    //   $table = $tnum * $num ;
+    //   echo "$tnum   *   $num =  $table <br/>";
+    //   $num = $num + 1;
+      
+    // }
+
+// 2. Do while loop
+    // do{
+    //   echo "$num .   Mozahid <br/>";
+    //   $num++;
+    // }while($num<=14.9)
+
+// 3.For Loop
+    for($num ; $num<=10 ; $num++){
+      echo "$num <br/>";
+    }
+
+?>
