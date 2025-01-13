f316e2a6a6f893eecf097ba32ba9461a4b5830ef
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-01-13 22:43:07 +0530

    I learn about concatination in String

---
+++ string/string.php
@@ -0,0 +1,11 @@
+<?php
+
+  $firstName = "Mozahid";
+  $lastName = " Izhar";
+  $fullName = $firstName.$lastName;
+  $age = 20;
+  $dob = " 12/06/2004";
+  echo $fullName.$dob;
+  // echo $dob;
+
+?>
