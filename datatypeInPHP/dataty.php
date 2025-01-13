122926793daec32a773c54150391a8cdfa8109b1
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-01-13 22:34:08 +0530

    i learn about variable if else and ternary operator

---
+++ datatypeInPHP/dataty.php
@@ -0,0 +1,11 @@
+<?Php
+    $x = "Mozahid";
+
+    var_dump($x);
+
+    // Const Variable  
+
+   define("num", 300);
+   echo num;
+
+?>
---
+++ logicalOperator/logic.php
@@ -0,0 +1,17 @@
+<?php
+    // $num = 100;
+    // $num1 = 60;
+    // if($num >= 10 && $num1 == 60){
+    //   echo "Yes i am Printed";
+    // }
+    // else{
+    //   echo "No i am nnot printed";
+    // }
+
+    // Ternary operator
+
+    $age = 22;
+
+    $isEligibility = ($age > 18 && $age < 30) ? "you can ride" : "you can't ride" ;
+    echo $isEligibility;
+?>
